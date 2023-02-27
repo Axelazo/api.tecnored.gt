58c6b29a2665b0043b3d9046343e4c5b1b2d0113
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-02-26 19:01:11 -0600

    Update routes.ts
    Added /users route to fetch all users
    Added /users/:id route to 1 user

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,6 @@
-import express, { Router, Request, Response } from "express";
+import express, { Router, Request, Response, request, response } from "express";
 import AuthController from "../controllers/AuthController";
+import UserController from "../controllers/UserController";
 import auth from "../middleware/auth";
 
 //* Main router
@@ -8,12 +9,16 @@ const router: Router = express.Router();
 //* Middlewares
 
 //* Controllers
-router.get("/users", auth, (request: Request, response: Response) => {
-  response.json({ hola: "mundo" });
-});
+/* router.get("/user/:id", auth, (request, response, next) => {
+  console.log(`[server]: GET request to /user ID: ${request.params.id}`);
+  response.json({ id: request.params.id });
+  next();
+}); */
+router.get("/users/", auth, UserController.getAllUsers);
+router.get("/users/:id", auth, UserController.getUserById);
 
 //* Home
-router.get("/", (request: Request, response: Response) => {
+router.get("/", (request, response) => {
   console.log(`[server]: ⚡️ Te amo mi amor, mi chiquitita preciosa ❤️❤️❤️`);
   return response.json({
     data: "⚡️ Te amo mi amor, mi chiquitita preciosa ❤️❤️❤️",
