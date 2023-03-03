1b86ba393fa73616ce689949c3ca0c5d20801d89
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-03-02 20:58:27 -0600

    Update server.ts
    Added cors middleware to allow requests from frontend app

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express, { Express } from "express";
 import router from "./routes/routes";
 import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
 import { sequelize } from "./models/index";
 
 dotenv.config({ path: __dirname + "/.env" });
@@ -8,11 +10,13 @@ dotenv.config({ path: __dirname + "/.env" });
 const app: Express = express();
 
 //Settings
-const port: number = parseInt(<string>process.env.PORT, 10) || 3000;
+const port: number = parseInt(<string>process.env.PORT, 10) || 4000;
 
 //Middlewares
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//app.use(morgan("combined"));
 
 //Routes
 app.use(router);
