6950d084e31fc4debc150bf0bf01353d8e8c9ce8
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-03-04 14:29:41 -0600

    Update routes.ts
    Added clients routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
-import express, { Router, Request, Response, request, response } from "express";
+import express, { Router } from "express";
 import AuthController from "../controllers/AuthController";
 import UserController from "../controllers/UserController";
+import ClientController from "../controllers/ClientController";
 import auth from "../middleware/auth";
 
 //* Main router
@@ -9,21 +10,11 @@ const router: Router = express.Router();
 //* Middlewares
 
 //* Controllers
-/* router.get("/user/:id", auth, (request, response, next) => {
-  console.log(`[server]: GET request to /user ID: ${request.params.id}`);
-  response.json({ id: request.params.id });
-  next();
-}); */
+router.get("/clients", ClientController.getAllClients);
+router.post("/clients/create", ClientController.createClient);
+
 router.get("/users/", auth, UserController.getAllUsers);
 router.get("/users/:id", auth, UserController.getUserById);
-
-//* Home
-router.get("/", (request, response) => {
-  console.log(`[server]: ⚡️ Te amo mi amor, mi chiquitita preciosa ❤️❤️❤️`);
-  return response.json({
-    data: "⚡️ Te amo mi amor, mi chiquitita preciosa ❤️❤️❤️",
-  });
-});
 
 //* Auth
 router.post("/signin", AuthController.signIn);
