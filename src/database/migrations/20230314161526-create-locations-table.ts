adfbb86054c0ef18247916cbb777e671d9cef424
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-03-14 18:41:27 -0600

    Added migrations
    Migration for table Services Address
    Migration for table Services Address Locations

---
+++ src/database/migrations/20230314161526-create-locations-table.ts
@@ -0,0 +1,42 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+module.exports = {
+  up: (queryInterface: QueryInterface): Promise<void> =>
+    queryInterface.createTable("serviceslocations", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        unique: true,
+      },
+      latitude: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      longitude: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      addressId: {
+        allowNull: true,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "servicesaddresses",
+          key: "id",
+        },
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+      },
+    }),
+
+  down: (queryInterface: QueryInterface): Promise<void> =>
+    queryInterface.dropTable("serviceslocations"),
+};
