067d239741fed7b381fe4a788472eae89f003197
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-03-22 11:31:00 -0600

    Update index.ts
    Updated routes to use catalogue routes (departments, notifications, statuses, etc)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,15 @@
 import express, { Router } from "express";
 import clientRoutes from "./clients";
+import catalogue from "./catalogue";
 import authRoutes from "./auth";
 
 const router: Router = express.Router();
 
+//app routes
 router.use("/clients", clientRoutes);
 router.use(authRoutes);
+
+//catalogue routes
+router.use("/", catalogue);
 
 export default router;
