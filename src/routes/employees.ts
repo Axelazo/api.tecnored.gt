296b6db0f2004f25d75cbc9161b0cab3864bc242
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-07-03 19:48:07 -0600

    Update employees.ts
    Added routes for getting employee by ID and creating a new employee, along with necessary middleware for handling file uploads

--- src/routes/employees.ts
+++ src/routes/employees.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import Auth from "../middleware/auth";
 import { RoleInterface } from "../ts/interfaces/app-interfaces";
 import EmployeeController from "../controllers/EmployeeController";
+import upload from "../middleware/upload";
 
 const allowedRoles: RoleInterface[] = [
   {
@@ -17,6 +18,25 @@ router.get(
   Auth.authenticate,
   Auth.checkRoles(allowedRoles),
   EmployeeController.getAllEmployees
+);
+
+router.get(
+  "/:id",
+  Auth.authenticate,
+  Auth.checkRoles(allowedRoles),
+  EmployeeController.getEmployeeById
+);
+
+router.post(
+  "/create",
+  upload.fields([
+    { name: "dpiFront", maxCount: 1 },
+    { name: "dpiBack", maxCount: 1 },
+    { name: "profilePicture", maxCount: 1 },
+  ]),
+  Auth.authenticate,
+  Auth.checkRoles(allowedRoles),
+  EmployeeController.createEmployee
 );
 
 export default router;
