45e961e897a4e53d68571adee30e93018700fba7
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-08-03 19:05:31 -0600

    Update 20230724181640-insert-plans.ts
    Removed unused fields

--- src/database/seeders/20230724181640-insert-plans.ts
+++ src/database/seeders/20230724181640-insert-plans.ts
@@ -5,30 +5,18 @@ module.exports = {
     return Promise.all([
       queryInterface.bulkInsert("plans", [
         {
-          name: "Plan Basico",
-          speed: 5,
-          realSpeed: 4.5,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          name: "Plan Estandar",
-          speed: 10,
-          realSpeed: 9.0,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          name: "Plan Avanzado",
-          speed: 15,
-          realSpeed: 13.5,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          name: "Plan Gaming",
-          speed: 20,
-          realSpeed: 18.0,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
