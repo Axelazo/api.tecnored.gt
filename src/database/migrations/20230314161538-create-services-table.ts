e19f009db3174efde21b6ba27441dba8013ae113
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-08-03 19:05:16 -0600

    Update 20230314161538-create-services-table.ts
    Removed unused fields

--- src/database/migrations/20230314161538-create-services-table.ts
+++ src/database/migrations/20230314161538-create-services-table.ts
@@ -18,27 +18,11 @@ module.exports = {
         allowNull: true,
         type: DataTypes.STRING,
       },
-      planId: {
-        allowNull: false,
-        type: DataTypes.INTEGER,
-        references: {
-          model: "plans",
-          key: "id",
-        },
-      },
       addressId: {
         allowNull: false,
         type: DataTypes.INTEGER,
         references: {
           model: "servicesaddresses",
-          key: "id",
-        },
-      },
-      statusId: {
-        allowNull: false,
-        type: DataTypes.INTEGER,
-        references: {
-          model: "statuses",
           key: "id",
         },
       },
