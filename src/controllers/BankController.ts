3ed9c4ad1dc088ef918bdeb0b29eb0d01f3e8df5
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-08-09 21:18:26 -0600

    Update BankController.ts
    Added Create, Update, and Delete methods for Banks catalogue

--- src/controllers/BankController.ts
+++ src/controllers/BankController.ts
@@ -1,7 +1,40 @@
 import { Response } from "express";
+import { sequelize } from "../models";
+import { Transaction } from "sequelize";
 import { AuthRequest } from "../ts/interfaces/app-interfaces";
 import Bank from "../models/Bank";
 import Account from "../models/Account";
+
+export const createBank = async (request: AuthRequest, response: Response) => {
+  const { name } = request.body;
+  try {
+    await sequelize.transaction(async (t: Transaction) => {
+      const existingBank = await Bank.findOne({ where: { name } });
+
+      if (existingBank) {
+        const message = `El banco ya existe!`;
+        response.status(409).json({ message });
+        return;
+      }
+
+      if (!name) {
+        const message = `El nombre del banco es requerido!`;
+        response.status(422).json({ message });
+        return;
+      }
+
+      const newBank = await Bank.create({ name }, { transaction: t });
+
+      if (newBank) {
+        response.status(200).json({ id: newBank.id });
+        return;
+      }
+    });
+  } catch (error) {
+    response.status(500).json(error);
+    return;
+  }
+};
 
 export const getAllBanks = async (request: AuthRequest, response: Response) => {
   try {
@@ -11,6 +44,70 @@ export const getAllBanks = async (request: AuthRequest, response: Response) => {
   } catch (error) {
     console.log(error);
     response.status(500).json(error);
+  }
+};
+
+export const updateBank = async (request: AuthRequest, response: Response) => {
+  const { id } = request.params;
+  const { name } = request.body;
+
+  try {
+    await sequelize.transaction(async (t: Transaction) => {
+      const bank = await Bank.findByPk(id); // Find the bank by its id
+
+      if (!bank) {
+        const message = "El banco solicitado no ha sido encontrado!";
+        return response.status(404).json({ message });
+      }
+
+      if (bank.name === name) {
+        const message = "El nombre del banco ya existe!";
+
+        return response.status(409).json({ message });
+      }
+
+      await bank.update({ name }, { transaction: t });
+
+      response.status(200).json({ id: bank.id });
+    });
+  } catch (error) {
+    response.status(500).json(error);
+  }
+};
+
+export const deleteBank = async (request: AuthRequest, response: Response) => {
+  const { id } = request.params;
+
+  try {
+    await sequelize.transaction(async (t: Transaction) => {
+      const bank = await Bank.findByPk(id, {
+        include: [{ model: Account, as: "accounts" }],
+      });
+
+      if (!bank) {
+        const message = `El banco solicitado no ha sido encontrado!`;
+        response.status(404).json({ message });
+        return;
+      }
+
+      if (bank.accounts && bank.accounts.length > 0) {
+        await Account.update(
+          { bankId: bank.id },
+          { where: { bankId: bank.id }, transaction: t }
+        );
+      }
+
+      await bank.destroy({ transaction: t }); // Delete the bank
+
+      response
+        .status(200)
+        .json({ message: "El banco ha sido borrado satisfactoriamente!" });
+    });
+  } catch (error) {
+    const message = `La transacción falló: Error ${error}`;
+    response.status(500).json({
+      message,
+    });
   }
 };
 
@@ -34,6 +131,9 @@ export const getAllAccountsFromBank = async (
 };
 
 export default {
+  createBank,
   getAllBanks,
+  updateBank,
+  deleteBank,
   getAllAccountsFromBank,
 };
