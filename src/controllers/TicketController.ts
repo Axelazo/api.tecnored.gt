82a7b1b70640367c64b429c66609d12d2fdf669b
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-08-22 21:26:50 -0600

    feat: added function createTicketForService
    Creates a Ticket entry in the database system, further entries are required

--- src/controllers/TicketController.ts
+++ src/controllers/TicketController.ts
@@ -1,12 +1,37 @@
 import { Response } from "express";
 import { AuthRequest } from "../ts/interfaces/app-interfaces";
-import { Transaction } from "sequelize";
-import { sequelize } from "../models";
 import Ticket from "../models/Ticket";
 import TicketStatuses from "../models/TicketStatuses";
 import TicketAssignees from "../models/TicketAssignees";
 import Employee from "../models/Employee";
 import User from "../models/User";
+
+export const createTicketForService = async (
+  request: AuthRequest,
+  response: Response
+) => {
+  try {
+    const tickets = await Ticket.findAll({
+      include: [
+        {
+          model: TicketStatuses,
+          as: "statuses",
+        },
+      ],
+    });
+
+    const ticketsAmount = tickets.length;
+
+    if (ticketsAmount > 0) {
+      response.status(200).json({ data: tickets });
+    } else {
+      response.status(204).json({ message: "No se ha encontrado ningun plan" });
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).json(error);
+  }
+};
 
 // Used for populating Tickets -> Admin
 export const getAllTickets = async (
@@ -77,4 +102,4 @@ export const getAllTicketsForEmployee = async (
   }
 };
 
-export default { getAllTickets };
+export default { getAllTickets, getAllTicketsForEmployee };
