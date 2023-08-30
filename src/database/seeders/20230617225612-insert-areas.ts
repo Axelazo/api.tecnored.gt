73ebfcc70cda08be931c90f7e6d2f068e41716a2
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-08-29 19:14:06 -0600

    fix: modified seeders to insert new schema

--- src/database/seeders/20230617225612-insert-areas.ts
+++ src/database/seeders/20230617225612-insert-areas.ts
@@ -5,55 +5,21 @@ module.exports = {
     return Promise.all([
       queryInterface.bulkInsert("areas", [
         {
-          establishmentId: 1,
           name: "Ingeniería",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          establishmentId: 1,
           name: "Ventas",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          establishmentId: 2,
-          name: "Ingeniería",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          establishmentId: 2,
-          name: "Ventas",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          establishmentId: 2,
-          name: "Ventas en ruta",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          establishmentId: 3,
-          name: "Ingeniería",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          establishmentId: 3,
-          name: "Ventas",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          establishmentId: 3,
           name: "Servicio al Cliente",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          establishmentId: 3,
           name: "Contabilidad",
           createdAt: new Date(),
           updatedAt: new Date(),
