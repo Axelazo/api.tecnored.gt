bc3a7e48ef426c669e8f6e279a13a5f86ab1f7c8
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-09-18 16:46:29 -0600

    feat: created Ticket Reason schema definition migration file

---
+++ src/database/migrations/20230810084922-create-ticket-reasons-table.ts
@@ -0,0 +1,29 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+module.exports = {
+  up: (queryInterface: QueryInterface): Promise<void> =>
+    queryInterface.createTable("ticketReasons", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+      },
+    }),
+
+  down: (queryInterface: QueryInterface): Promise<void> =>
+    queryInterface.dropTable("ticketReasons"),
+};
