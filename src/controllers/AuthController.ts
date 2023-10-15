6e54744d4710bbd912d710e969404855947c9dae
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-10-15 09:35:58 -0600

    feat: improved readability of LogIn function

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,26 +2,30 @@ import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import auth from "../config/auth";
-import User from "../models/User";
-import Role from "../models/Role";
+import { User, Role, Employee } from "../models/Relationships";
 
-function signIn(request: Request, response: Response) {
+/* function signIn(request: Request, response: Response) {
   const { email, password } = request.body;
 
   User.findOne({
     where: {
       email: email,
     },
-    include: {
-      model: Role,
-      as: "roles",
-      through: {
-        attributes: [],
+    include: [
+      {
+        model: Role,
+        as: "roles",
+        through: {
+          attributes: [],
+        },
       },
-    },
+      {
+        model: Employee,
+        as: "employee",
+      },
+    ],
   })
     .then((user) => {
-      console.log(user?.email);
       if (!user) {
         response.status(400).json({ message: "Usuario no encontrado" });
       } else {
@@ -53,6 +57,77 @@ function signIn(request: Request, response: Response) {
       console.log(error);
       response.status(500).json(error);
     });
+} */
+
+async function signIn(request: Request, response: Response) {
+  const { email, password } = request.body;
+
+  if (!email) {
+    return response
+      .status(409)
+      .json({ message: "El correo electrónico es requerido" });
+  }
+
+  if (!password) {
+    return response.status(409).json({ message: "La contraseña requerida" });
+  }
+
+  try {
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+      include: [
+        {
+          model: Role,
+          as: "roles",
+          through: {
+            attributes: [],
+          },
+        },
+        {
+          model: Employee,
+          as: "employee",
+          attributes: ["id", "employeeNumber"],
+        },
+      ],
+    });
+
+    if (!user) {
+      return response
+        .status(401)
+        .json({ message: "Correo electrónico o contraseña incorrecta" });
+    }
+
+    const result = await bcrypt.compare(password, user.password);
+
+    if (!result) {
+      return response
+        .status(401)
+        .json({ message: "Correo electrónico o contraseña incorrecta" });
+    }
+
+    const { id, firstNames, lastNames, roles, employee } = user;
+
+    const token = jwt.sign({ user: user }, auth.secret, {
+      expiresIn: auth.expires,
+    });
+
+    return response.status(200).json({
+      data: {
+        id,
+        firstNames,
+        lastNames,
+        email,
+        roles,
+        employeeId: employee?.id || null,
+        token,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ message: error });
+  }
 }
 
 //Registration for new user
