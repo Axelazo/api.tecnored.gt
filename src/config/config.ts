8ebb572bdaaa4ab492f6674c01c06cd30a69595f
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-10-21 10:30:04 -0600

    fix: specify bind address for deployment

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ const config: Options = {
   username: process.env.DB_USERNAME || "root",
   password: process.env.DB_PASSWORD || "Axelazo123!",
   database: process.env.DB_DATABASE || "tecnoredms",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "127.0.0.1",
   dialect: (process.env.DB_DIALECT as Dialect) || "mysql",
   logging: false,
   timezone: "-06:00",
