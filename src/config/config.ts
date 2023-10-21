bf48fe7fe1defa2be2cd5a28e5862df4d0fec83f
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-10-21 10:48:13 -0600

    fix: changed port and password for deployment

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,9 +5,9 @@ dotenv.config({ path: __dirname + "../../.env" });
 
 const config: Options = {
   username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || undefined,
+  password: process.env.DB_PASSWORD || "Axelazo123!",
   database: process.env.DB_DATABASE || "tecnoredms",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "127.0.0.1",
   dialect: (process.env.DB_DIALECT as Dialect) || "mysql",
   logging: false,
   timezone: "-06:00",
